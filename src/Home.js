caa4e67d041959795aa51dc8411e9032bc09b25c
ManSheung Wong <wongmansheung@ManSheungde-MacBook-Air.local>
2022-05-30 00:46:56 +0800

    feature: Post Detail Button

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Grid, Row, Col, Button, Table } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+
+
 
 export default function Home() {
 
 
   const [postsList, setPostsList] = useState([])
+  const [modalShow, setModalShow] = useState(false)
+  const [postDetail, setPostDetail] = useState({
+    title: null,
+    body: null,
+  })
+
+  const handleClose = () => setModalShow(false);
 
   useEffect(() => {
 
@@ -19,14 +29,41 @@ export default function Home() {
 
   return (
     <div className="container">
+      <div>
+        <Modal
+          show={modalShow}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>{postDetail.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{postDetail.body}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
 
       {
-        postsList.map(post => (
-          <div key={post.id} className="card" style={{ margin: "20px 0" }}>
-            <h5 className="card-header">{post.title}</h5>
+        postsList.map(currentPost => (
+
+          <div key={currentPost.id} className="card" style={{ margin: "20px 0" }}>
+            <h5 className="card-header">{currentPost.title}</h5>
             <div className="card-body">
-              <p className="card-text">{post.body}</p>
-              <a href="#" className="btn btn-primary">Detail</a>
+              <p className="card-text multiline-ellipsis" >{currentPost.body}</p>
+
+              <Button variant="primary" onClick={
+                () => {
+                  setModalShow(true)
+                  setPostDetail(currentPost)
+                }
+              }>
+                Detail
+              </Button>
             </div>
           </div>
         ))
